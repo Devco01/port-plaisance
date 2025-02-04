9f1dcef89dd6f185b70089fa3bf05cde8e209f93
Devco01 <pepsiman15.1@gmail.com>
2025-02-04 21:54:16 +0100

    fix: eslint configuration to ignore client files

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+        es6: true,
+        jest: true
+    },
+    extends: [
+        'eslint:recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
+    },
+    ignorePatterns: [
+        'client/**/*',
+        'build/**',
+        'dist/**',
+        'coverage/**',
+        'node_modules/**',
+        '*.config.js'
+    ],
+    rules: {
+        'indent': ['error', 4],
+        'linebreak-style': ['off'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always']
+    }
+}; 
--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,13 @@ module.exports = {
     testTimeout: 30000,
     setupFilesAfterEnv: ['<rootDir>/tests/setup/jest.setup.js'],
     globalTeardown: '<rootDir>/tests/setup/teardown.js',
-    testPathIgnorePatterns: ['/node_modules/'],
+    testPathIgnorePatterns: [
+        '/node_modules/',
+        '/client/'
+    ],
+    transform: {
+        '^.+\\.js$': 'babel-jest'
+    },
     forceExit: true,
     detectOpenHandles: true,
     verbose: true
